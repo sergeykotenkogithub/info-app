c6999db919e060ec0eac875ca4b4b8a663efc23e
sergeykotenko <sergeykotenko@bk.ru>
2023-02-06 19:01:44 +0300

    Feat(Add Navbar)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,26 +1,19 @@
 import { FC } from 'react'
 import './styles/index.scss'
-import { Route, Routes } from 'react-router-dom'
-import { Link } from 'react-router-dom'
-import { Suspense } from 'react'
+
+import { AppRouter } from './providers/router'
+
 import { classNames } from 'shared/lib/classNames/classNames'
 import { useTheme } from 'app/providers/ThemeProvider'
-import { AboutPage } from 'pages/AboutPage'
-import { MainPage } from 'pages/MainPage'
+import { Navbar } from 'widgets/Navbar'
 
 const App: FC = () => {
   const { theme, toggleTheme } = useTheme()
   return (
     <div className={classNames('app', {}, [theme])}>
+      <Navbar />
+      <AppRouter />
       <button onClick={toggleTheme}>Change</button>
-      <Link to={'/'}>Главная</Link>
-      <Link to={'/about'}>О сайте</Link>
-      <Suspense fallback={<div>Loading....</div>}>
-        <Routes>
-          <Route path={'/about'} element={<AboutPage />} />
-          <Route path={'/'} element={<MainPage />} />
-        </Routes>
-      </Suspense>
     </div>
   )
 }
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -2,12 +2,12 @@ type Mods = Record<string, boolean | string>
 
 export function classNames(
   cls: string,
-  mods: Mods,
-  addition: string[]
+  mods: Mods = {},
+  additional: string[] = []
 ): string {
   return [
     cls,
-    ...addition,
+    ...additional.filter(Boolean),
     Object.entries(mods)
       .filter(([className, value]) => Boolean(value))
       .map(([classNames]) => classNames),
