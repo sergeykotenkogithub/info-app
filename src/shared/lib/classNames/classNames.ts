05e4eaaf25ddad4d5e986209759d3679ec6883b6
sergeykotenko <sergeykotenko@bk.ru>
2023-02-07 20:19:00 +0300

    Feat(add jest, add first test)

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -8,7 +8,7 @@ export function classNames(
   return [
     cls,
     ...additional.filter(Boolean),
-    Object.entries(mods)
+    ...Object.entries(mods)
       .filter(([_, value]) => Boolean(value))
       .map(([className]) => className),
   ].join(' ')
