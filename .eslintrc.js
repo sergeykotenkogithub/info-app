531f466149d0839501dcf3b57e91d8e42fcd5053
sergeykotenko <sergeykotenko@bk.ru>
2023-02-07 17:05:55 +0300

    Feat(add stylelint, plugin for eslint) Fix(style)

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,19 @@ module.exports = {
     es2021: true,
     jest: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'airbnb-typescript'],
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'plugin:i18next/recommended',
+  ],
   overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
     project: './tsconfig.json',
   },
-  plugins: ['react'],
+  plugins: ['react', 'i18next'],
   rules: {
     'react/jsx-indent': [2, 2],
     'react/jsx-indent-props': [2, 2],
@@ -44,6 +49,10 @@ module.exports = {
     'import/no-extraneous-dependencies': 'off',
     'no-underscore-dangle': 'off',
     '@typescript-eslint/naming-convention': 'warn',
+    'i18next/no-literal-string': [
+      'error',
+      { markupOnly: true, ignoreAttribute: ['stroke'] },
+    ],
   },
   globals: {
     __IS_DEV__: true,
--- src/features/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/features/ui/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -3,7 +3,6 @@ import { Theme, useTheme } from 'app/providers/ThemeProvider'
 import { Button, ThemeButton } from 'shared/ui/Button/Button'
 import LightIcon from 'shared/assets/icons/theme-light.svg'
 import DarkIcon from 'shared/assets/icons/theme-dark.svg'
-import cls from './ThemeSwitcher.module.scss'
 
 interface ThemeSwitcherProps {
   className?: string
@@ -14,7 +13,7 @@ export const ThemeSwitcher = ({ className }: ThemeSwitcherProps) => {
 
   return (
     <Button
-      className={classNames(cls.ThemeSwitcher, {}, [className])}
+      className={classNames('', {}, [className])}
       onClick={toggleTheme}
       theme={ThemeButton.CLEAR}
     >
