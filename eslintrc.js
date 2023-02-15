f3ae97e2244443951c046746288b3f84e621b83c
sergeykotenko <sergeykotenko@bk.ru>
2023-02-15 18:22:29 +0300

    Feat(add checks )

---
+++ eslintrc.js
@@ -0,0 +1,81 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'plugin:i18next/recommended',
+    'plugin:storybook/recommended',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react', 'i18next'],
+  rules: {
+    'react/jsx-indent': [2, 2],
+    'react/jsx-indent-props': [2, 2],
+    indent: [2, 2],
+    semi: 'off',
+    '@typescript-eslint/semi': 'off',
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'import/prefer-default-export': 'off',
+    'react/jsx-filename-extension': [
+      2,
+      {
+        extensions: ['.js', '.jsx', '.tsx'],
+      },
+    ],
+    'import/no-unresolved': 'off',
+    'no-unused-vars': 'warn',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/comma-dangle': 'off',
+    'react/require-default-props': 'off',
+    'object-curly-newline': 'off',
+    'react/jsx-props-no-spreading': 'warn',
+    'no-shadow': 'off',
+    'react/prop-types': 'off',
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'no-underscore-dangle': 'off',
+    '@typescript-eslint/naming-convention': 'warn',
+    'i18next/no-literal-string': [
+      'error',
+      {
+        markupOnly: true,
+        ignoreAttribute: ['to', 'data-testid'],
+      },
+    ],
+    'max-len': [
+      'error',
+      {
+        ignoreComments: true,
+        code: 100,
+      },
+    ],
+  },
+  globals: {
+    __IS_DEV__: true,
+  },
+  overrides: [
+    {
+      files: ['**/src/**/*.test.{ts,tsx}'],
+      rules: {
+        'i18next/no-literal-string': 'off',
+      },
+    },
+  ],
+}
