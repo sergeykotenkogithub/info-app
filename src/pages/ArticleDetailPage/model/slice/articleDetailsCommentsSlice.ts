d764205eadc7e76810c11efb97021779169dd93c
sergeykotenko <sergeykotenko@bk.ru>
2023-03-16 20:07:02 +0300

    Feat(Comment list)

---
+++ src/pages/ArticleDetailPage/model/slice/articleDetailsCommentsSlice.ts
@@ -0,0 +1,50 @@
+import {
+  PayloadAction,
+  createEntityAdapter,
+  createSlice,
+} from '@reduxjs/toolkit'
+import { StateSchema } from 'app/providers/StoreProvider'
+import { Comment } from 'entities/Comment'
+import { fetchCommentByArticleId } from '../services/fetchCommentByArticleId'
+import { ArticleDetailsCommentsSchema } from '../types/ArticleDetailsCommentsSchema'
+
+const commentsAdapter = createEntityAdapter<Comment>({
+  selectId: (comment) => comment.id,
+})
+
+export const getArticleComments = commentsAdapter.getSelectors<StateSchema>(
+  (state) => state.articleDetailsComments || commentsAdapter.getInitialState()
+)
+
+const articleDetailsCommentsSlice = createSlice({
+  name: 'articleDetailsCommentsSlice',
+  initialState: commentsAdapter.getInitialState<ArticleDetailsCommentsSchema>({
+    isLoading: false,
+    ids: [],
+    entities: {},
+    error: undefined,
+  }),
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCommentByArticleId.pending, (state) => {
+        state.error = undefined
+        state.isLoading = true
+      })
+      .addCase(
+        fetchCommentByArticleId.fulfilled,
+        (state, action: PayloadAction<Comment[]>) => {
+          state.isLoading = false
+          commentsAdapter.setAll(state, action.payload)
+        }
+      )
+      .addCase(fetchCommentByArticleId.rejected, (state, action) => {
+        state.isLoading = false
+        state.error = action.payload
+      })
+  },
+})
+
+// eslint-disable-next-line operator-linebreak
+export const { reducer: articleDetailsCommentsReducer } =
+  articleDetailsCommentsSlice
--- src/pages/ArticleDetailPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -1,18 +1,43 @@
 import { ArticleDetails } from 'entities/Article'
+import { CommentList } from 'entities/Comment'
+import { fetchCommentByArticleId } from 'pages/ArticleDetailPage/model/services/fetchCommentByArticleId'
 import { memo } from 'react'
 import { useTranslation } from 'react-i18next'
+import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
 import { classNames } from 'shared/lib/classNames/classNames'
+import {
+  DynamicModuleLoader,
+  ReducersList,
+} from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader'
+import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect'
+import { Text } from 'shared/ui/Text/Text'
+import { getArticleCommentsIsLoading } from '../../model/selectors/comments'
+import {
+  articleDetailsCommentsReducer,
+  getArticleComments,
+} from '../../model/slice/articleDetailsCommentsSlice'
 import cls from './ArticleDetailsPage.module.scss'
 
 interface ArticleDetailPageProps {
   className?: string
 }
 
+const reducers: ReducersList = {
+  articleDetailsComments: articleDetailsCommentsReducer,
+}
+
 const ArticleDetailsPage = (props: ArticleDetailPageProps) => {
   const { className } = props
   const { t } = useTranslation('article-details')
   const { id } = useParams<{ id: string }>()
+  const dispatch = useDispatch()
+  const comments = useSelector(getArticleComments.selectAll)
+  const commentsIsLoading = useSelector(getArticleCommentsIsLoading)
+
+  useInitialEffect(() => {
+    dispatch(fetchCommentByArticleId(id))
+  })
 
   if (!id) {
     return (
@@ -23,9 +48,13 @@ const ArticleDetailsPage = (props: ArticleDetailPageProps) => {
   }
 
   return (
-    <div className={classNames(cls.articleDetailPage, {}, [className])}>
-      <ArticleDetails id={id} />
-    </div>
+    <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
+      <div className={classNames(cls.articleDetailPage, {}, [className])}>
+        <ArticleDetails id={id} />
+        <Text className={cls.commentTitle} title={t('a-comment')} />
+        <CommentList isLoading={commentsIsLoading} comments={comments} />
+      </div>
+    </DynamicModuleLoader>
   )
 }
 
