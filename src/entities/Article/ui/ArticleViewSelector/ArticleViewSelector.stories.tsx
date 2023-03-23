7de5e111e1d64355dfbfa34d260ba48dc6597b86
sergeykotenko <sergeykotenko@bk.ru>
2023-03-23 19:20:08 +0300

    Fix(storybook articles)

--- src/entities/Article/ui/ArticleViewSelector/ArticleViewSelector.stories.tsx
+++ src/entities/Article/ui/ArticleViewSelector/ArticleViewSelector.stories.tsx
@@ -1,4 +1,4 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
 
 import { ArticleViewSelector } from './ArticleViewSelector'
 
@@ -10,7 +10,9 @@ export default {
   },
 } as ComponentMeta<typeof ArticleViewSelector>
 
-const Template: ComponentStory<typeof ArticleViewSelector> = (args) => <ArticleViewSelector {...args} />
+const Template: ComponentStory<typeof ArticleViewSelector> = (args) => (
+  <ArticleViewSelector {...args} />
+)
 
 export const Normal = Template.bind({})
 Normal.args = {}
