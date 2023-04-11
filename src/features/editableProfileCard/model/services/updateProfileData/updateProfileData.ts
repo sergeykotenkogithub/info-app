78a52f49e643b844b59039b80f033428b3656e5f
sergeykotenko <sergeykotenko@bk.ru>
2023-04-11 17:42:44 +0300

    Fix(lint ts)

--- src/features/editableProfileCard/model/services/updateProfileData/updateProfileData.ts
+++ src/features/editableProfileCard/model/services/updateProfileData/updateProfileData.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/indent */
 import { createAsyncThunk } from '@reduxjs/toolkit'
 import { ThunkConfig } from 'app/providers/StoreProvider'
 import { Profile } from 'entities/Profile'
