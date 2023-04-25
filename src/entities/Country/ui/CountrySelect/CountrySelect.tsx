db11ac43acc841907fe636aaedd4eb559fb73bf5
sergeykotenko <sergeykotenko@bk.ru>
2023-04-25 20:40:10 +0300

    update custom plugin eslint

--- src/entities/Country/ui/CountrySelect/CountrySelect.tsx
+++ src/entities/Country/ui/CountrySelect/CountrySelect.tsx
@@ -1,6 +1,6 @@
+import { ListBox } from '@/shared/ui/Popups'
 import { FC, memo, useCallback } from 'react'
 import { useTranslation } from 'react-i18next'
-import { ListBox } from '@/shared/ui/Popups'
 import { Country } from '../../model/types/country'
 
 interface CountrySelectProps {
