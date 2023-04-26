9e4af8fd7fa9933e56ca4264b3f3d5624044cf3a
sergeykotenko <sergeykotenko@bk.ru>
2023-04-26 16:05:18 +0300

    upgrade webpack config, add babel cache

--- config/build/loaders/buildBabelLoader.ts
+++ config/build/loaders/buildBabelLoader.ts
@@ -1,3 +1,5 @@
+/* eslint-disable indent */
+/* eslint-disable @typescript-eslint/indent */
 import babelRemovePropsPlugin from '../../babel/babelRemovePropsPlugin'
 import { BuildOptions } from '../types/config'
 
@@ -6,29 +8,32 @@ interface BuildBabelLoaderProps extends BuildOptions {
 }
 
 export function buildBabelLoader({ isDev, isTsx }: BuildBabelLoaderProps) {
+  const isProd = !isDev
   return {
     test: isTsx ? /\.(jsx|tsx)$/ : /\.(js|ts)$/,
     exclude: /node_modules/,
     use: {
       loader: 'babel-loader',
       options: {
+        cacheDirectory: true,
         presets: ['@babel/preset-env'],
         plugins: [
-          [
-            'i18next-extract',
-            {
-              locales: ['en', 'ru'],
-              keyAsDefaultValue: true,
-            },
-          ],
+          // [
+          //   'i18next-extract',
+          //   {
+          //     locales: ['en', 'ru'],
+          //     keyAsDefaultValue: true,
+          //   },
+          // ],
           ['@babel/plugin-transform-typescript', { isTsx }],
           '@babel/plugin-transform-runtime',
-          isTsx && [
-            babelRemovePropsPlugin,
-            {
-              props: ['data-testid'],
-            },
-          ],
+          isTsx &&
+            isProd && [
+              babelRemovePropsPlugin,
+              {
+                props: ['data-testid'],
+              },
+            ],
           isDev && require.resolve('react-refresh/babel'),
         ].filter(Boolean),
       },
