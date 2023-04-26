e694a5b139f2a3df409ecd661715cf294ee4042d
sergeykotenko <sergeykotenko@bk.ru>
2023-04-26 13:41:54 +0300

    add theme addon

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,5 +1,4 @@
 import { addDecorator } from '@storybook/react'
-// import { Theme } from '../../src/app/providers/ThemeProvider'
 import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator'
 import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator'
 import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator'
@@ -16,6 +15,14 @@ export const parameters = {
     },
   },
   layout: 'fullscreen',
+  themes: {
+    default: 'light',
+    list: [
+      { name: 'light', class: Theme.LIGHT, color: '#ffffff' },
+      { name: 'dark', class: Theme.DARK, color: '#000000' },
+      { name: 'orange', class: Theme.ORANGE, color: '#ffb005' },
+    ],
+  },
 }
 
 addDecorator(StyleDecorator)
