f4bade75662ba69f7be01f2d708ec062b861919b
sergeykotenko <sergeykotenko@bk.ru>
2023-05-22 18:17:09 +0300

    fix icons size and color

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -5,10 +5,26 @@ import { BuildOptions } from './types/config'
 
 export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
   const { isDev } = options
+
   const svgLoader = {
-    test: /\.svg$/,
-    use: ['@svgr/webpack'],
-  }
+      test: /\.svg$/,
+      use: [{
+        loader: '@svgr/webpack',
+        options: {
+          icon: true,
+          svgoConfig: {
+            plugins: [
+              {
+                name: 'convertColors',
+                params: {
+                    currentColor: true,
+                }
+              }
+            ]
+          }
+        }
+      }],
+    };
 
   const codeBabelLoader = buildBabelLoader({ ...options, isTsx: false })
   const tsxCodeBabelLoader = buildBabelLoader({ ...options, isTsx: true })
